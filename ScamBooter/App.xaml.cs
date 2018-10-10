34dd176f92b9c9514b7921f44823604a52a03590
Ryan Koon <ryankoon@alumni.ubc.ca>
2018-10-09 23:04:23 -0700

    Trigger notification with button

---
+++ ScamBooter/App.xaml.cs
@@ -0,0 +1,18 @@
+﻿using System.Diagnostics;
+using System.Windows;
+using Application = System.Windows.Application;
+
+namespace ScamBooter
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            Debug.Print("Started!");
+        }
+    }
+}
---
+++ ScamBooter/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+using System.Windows;
+using System.Windows.Forms;
+
+namespace ScamBooter
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public static NotifyIcon nIcon = new NotifyIcon();
+        public MainWindow()
+        {
+            nIcon.Icon = new Icon(@"SB.ico");
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ToolStripMenuItem statusMenuItem = new ToolStripMenuItem("Status");
+            ToolStripMenuItem disableMenuItem = new ToolStripMenuItem("Disable");
+            ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit");
+            nIcon.Visible = true;
+            nIcon.ShowBalloonTip(5000, "Scam Attempt Detected", "Remote connections were disconnected for your protection.", ToolTipIcon.Info);
+            nIcon.ContextMenuStrip = new ContextMenuStrip();
+            nIcon.ContextMenuStrip.Items.AddRange(
+                new System.Windows.Forms.ToolStripItem[] {
+                statusMenuItem, disableMenuItem, exitMenuItem
+            });
+        }
+    }
+}
