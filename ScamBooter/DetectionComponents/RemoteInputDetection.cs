547f1de86b13fb250a84d552428a6fc8d7d4313b
Ryan Koon <ryankoon@alumni.ubc.ca>
2018-11-05 13:01:02 -0800

    Detect rdp connection Global mouse and keyboard hooks

---
+++ ScamBooter/DetectionComponents/RemoteInputDetection.cs
@@ -0,0 +1,61 @@
+﻿using GlobalLowLevelHooks;
+using System;
+using System.Diagnostics;
+
+namespace ScamBooter
+{
+    public class RemoteInputDetection
+    {
+        MouseHook mouseHook;
+        KeyboardHook keyboardHook;
+        public RemoteInputDetection()
+        {
+            mouseHook = new MouseHook();
+            keyboardHook = new KeyboardHook();
+        }
+        // Create the Mouse Hook
+
+        public void RegisterHooks()
+        {
+            // Capture mouse events
+            mouseHook.MouseMove += new MouseHook.MouseHookCallback(MouseHook_MouseMove);
+            mouseHook.Install();
+
+            // Capture keyboard events
+            keyboardHook.KeyDown += new KeyboardHook.KeyboardHookCallback(KeyboardHook_KeyDown);
+            keyboardHook.KeyUp += new KeyboardHook.KeyboardHookCallback(KeyboardHook_KeyUp);
+            keyboardHook.Install();
+
+            // Remove handlers on application close
+            AppDomain.CurrentDomain.ProcessExit += new EventHandler(OnProcessExit);
+        }
+
+        private void MouseHook_MouseMove(MouseHook.MSLLHOOKSTRUCT mouseStruct)
+        {
+            Debug.Print("Mouse x:" + mouseStruct.pt.x.ToString());
+            Debug.Print("Mouse y:" + mouseStruct.pt.y.ToString());
+        }
+
+        private void KeyboardHook_KeyDown(KeyboardHook.VKeys key)
+        {
+            Debug.Print("KeyDown V:" + key.Equals(KeyboardHook.VKeys.KEY_V));
+        }
+
+        private void KeyboardHook_KeyUp(KeyboardHook.VKeys key)
+        {
+            Debug.Print("KeyUp V:" + key.Equals(KeyboardHook.VKeys.KEY_V));
+        }
+
+        private void OnProcessExit(object sender, EventArgs e)
+        {
+            keyboardHook.KeyDown -= new KeyboardHook.KeyboardHookCallback(KeyboardHook_KeyDown);
+            keyboardHook.KeyUp -= new KeyboardHook.KeyboardHookCallback(KeyboardHook_KeyUp);
+            keyboardHook.Uninstall();
+            mouseHook.MouseMove -= new MouseHook.MouseHookCallback(MouseHook_MouseMove);
+            mouseHook.Uninstall();
+            Debug.Print("Uninstalled global hooks");
+        }
+
+
+    }
+}
--- ScamBooter/MainWindow.xaml.cs
+++ ScamBooter/MainWindow.xaml.cs
@@ -12,22 +12,46 @@ namespace ScamBooter
         public static NotifyIcon nIcon = new NotifyIcon();
         public MainWindow()
         {
-            nIcon.Icon = new Icon(@"SB.ico");
+            nIcon.Icon = new Icon(@"Resources\SB.ico");
             InitializeComponent();
+            RemoteInputDetection rid = new RemoteInputDetection();
+            rid.RegisterHooks();
         }
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
+            TriggerNotification("Scam Attempt Detected", "Remote connections were disconnected for your protection.", 5000);
+        }
+
+        private static void TriggerNotification(string header, string message, int timeout)
+        {
             ToolStripMenuItem statusMenuItem = new ToolStripMenuItem("Status");
             ToolStripMenuItem disableMenuItem = new ToolStripMenuItem("Disable");
             ToolStripMenuItem exitMenuItem = new ToolStripMenuItem("Exit");
+            nIcon.Icon = SystemIcons.Error;
             nIcon.Visible = true;
-            nIcon.ShowBalloonTip(5000, "Scam Attempt Detected", "Remote connections were disconnected for your protection.", ToolTipIcon.Info);
+            nIcon.ShowBalloonTip(timeout, header, message, ToolTipIcon.Info);
             nIcon.ContextMenuStrip = new ContextMenuStrip();
             nIcon.ContextMenuStrip.Items.AddRange(
                 new System.Windows.Forms.ToolStripItem[] {
                 statusMenuItem, disableMenuItem, exitMenuItem
             });
         }
+
+        private void Test_Remote(object sender, RoutedEventArgs e)
+        {
+            RemoteConnectionDetection rce = new RemoteConnectionDetection();
+            bool isRemoteConnection = rce.IsRemoteConnectionActive();
+
+            if (isRemoteConnection)
+            {
+                TriggerNotification("Remote Connection Detected", "unsafe", 5000);
+            }
+            else
+            {
+                TriggerNotification("No Remote Connections Detected", "safe", 5000);
+            }
+        }
+
     }
 }
